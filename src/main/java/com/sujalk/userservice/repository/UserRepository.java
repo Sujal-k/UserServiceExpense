6063a292388f7846bbc0ce43454c0721708e34dc
Sujal <sujalkesharwani677@gmail.com>
2025-05-17 18:05:03 +0530

    Add UserRepository interface

---
+++ src/main/java/com/sujalk/userservice/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.sujalk.userservice.repository;
+
+
+import com.sujalk.userservice.entities.UserInfo;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepository extends CrudRepository<UserInfo, String>
+{
+
+    Optional<UserInfo> findByUserId(String userId);
+
+}
